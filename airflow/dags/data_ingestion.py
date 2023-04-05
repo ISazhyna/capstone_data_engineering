52ddbeb3cfe501c9f05de734540d207ac9161edd
ISazhyna <seoirinas@gmail.com>
2023-04-05 10:57:47 -0500

    dags for temp 2022 and 2021

--- airflow/dags/data_ingestion.py
+++ airflow/dags/data_ingestion.py
@@ -32,7 +32,7 @@ def download_data_to_gcs(
 """
 New data is added every week, so we overwrite the existed file accordingly.
 """
-CRIME_URL = 'https://data.austintexas.gov/api/views/fdj4-gpfu/rows.csv?accessType=DOWNLOAD  '
+CRIME_URL = 'https://data.austintexas.gov/api/views/fdj4-gpfu/rows.csv?accessType=DOWNLOAD'
 CRIME_CSV_FILE = 'crime_austin.csv'
 
 crime_data_dag = DAG(
@@ -50,4 +50,52 @@ download_data_to_gcs(
     dataset='crime',
     download_url=CRIME_URL,
     csv_file=CRIME_CSV_FILE,
+)
+
+
+"""
+Historical measured data of temperature in Austin 2022
+"""
+
+TEMP_22_URL='\"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/retrievebulkdataset?&key=LS846U5QUC4XUPFSUSK44UR3T&taskId=eda09bd6d60dce86d4c043eb83e24ee4&zip=false\"'
+TEMP_22_CSV_FILE='temp_austin_2022.csv'
+
+temp_22_data_dag = DAG(
+    dag_id="temp_22_data_dag",
+    schedule_interval=None,
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=3,
+    tags=['temp_data'],
+)
+
+download_data_to_gcs(
+    dag=temp_22_data_dag,
+    dataset='crime',
+    download_url=TEMP_22_URL,
+    csv_file=TEMP_22_CSV_FILE,
+)
+
+
+"""
+Historical measured data of temperature in Austin 2021
+"""
+
+TEMP_21_URL='\"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/retrievebulkdataset?&key=LS846U5QUC4XUPFSUSK44UR3T&taskId=9ba67a57065456d5a95987d32495fb48&zip=false\"'
+TEMP_21_CSV_FILE='temp_austin_2021.csv'
+
+temp_21_data_dag = DAG(
+    dag_id="temp_21_data_dag",
+    schedule_interval=None,
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=3,
+    tags=['temp_data'],
+)
+
+download_data_to_gcs(
+    dag=temp_21_data_dag,
+    dataset='crime',
+    download_url=TEMP_21_URL,
+    csv_file=TEMP_21_CSV_FILE,
 )
